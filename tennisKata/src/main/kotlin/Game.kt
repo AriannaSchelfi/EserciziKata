d1a89145f10f49f4d65e849a316c8a06333b8bcd
AriannaSchelfi <arianna.schelfi@hotmail.it>
2021-09-07 15:30:57 +0200

    tennisKata

---
+++ tennisKata/src/main/kotlin/Game.kt
@@ -0,0 +1,58 @@
+class Game (private var namePlayer1: String, private var namePlayer2: String){
+    private var scorePlayer1 : Int = 0
+    private var scorePlayer2 : Int = 0
+
+    fun getScore() : String{
+        return if (scorePlayer1==4 && scorePlayer2<3){
+            "$namePlayer1 win!"
+        } else if (scorePlayer1<3 && scorePlayer2==4){
+            "$namePlayer2 win!"
+        } else if(scorePlayer1>=3 && scorePlayer2>=3){
+            vantaggi()
+        } else {
+            punteggio()
+        }
+    }
+
+    private fun punteggio() =
+        "$namePlayer1 : ${translateScore(scorePlayer1)} - $namePlayer2 : ${translateScore(scorePlayer2)}"
+
+    private fun vantaggi(): String {
+        return if (scorePlayer1 == scorePlayer2 + 1) {
+            "advantage $namePlayer1"
+        } else if (scorePlayer1 + 1 == scorePlayer2) {
+            "advantage $namePlayer2"
+        } else if (scorePlayer1 == scorePlayer2) {
+            "deuce"
+        } else{
+            vittoria()
+        }
+    }
+
+    private fun vittoria(): String {
+        return if (scorePlayer1 > scorePlayer2) {
+            "$namePlayer1 win!"
+        } else {
+            "$namePlayer2 win!"
+        }
+    }
+
+    private fun translateScore(score : Int) : String {
+        return when(score) {
+            0 -> "love"
+            1 -> "fifteen"
+            2 -> "thirty"
+            3 -> "forty"
+            else -> "errore"
+        }
+
+    }
+
+    fun newPoint(giocatore: String) {
+        if (giocatore == namePlayer1) {
+            scorePlayer1++
+        } else{
+            scorePlayer2++
+        }
+    }
+}
---
+++ tennisKata/src/test/kotlin/GameTest.kt
@@ -0,0 +1,94 @@
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class GameTest{
+
+    @Test
+    fun lovePerPunteggioIniziale() {
+        val gioco = Game("Nadal", "Djokovic")
+        val score = gioco.getScore()
+        assertEquals("Nadal : love - Djokovic : love", score)
+    }
+
+    @Test
+    fun fifteenPerPunteggioUno() {
+        val gioco = Game("Nadal", "Djokovic")
+        gioco.newPoint("Nadal")
+        gioco.newPoint("Djokovic")
+        assertEquals("Nadal : fifteen - Djokovic : fifteen", gioco.getScore())
+    }
+
+    @Test
+    fun thirtyPerPunteggioDue() {
+        val gioco = Game("Nadal", "Djokovic")
+        gioco.newPoint("Nadal")
+        gioco.newPoint("Nadal")
+        assertEquals("Nadal : thirty - Djokovic : love", gioco.getScore())
+    }
+
+    @Test
+    fun fortyPerPunteggioTre() {
+        val gioco = Game("Nadal", "Djokovic")
+        for(i in 0..2){
+            gioco.newPoint("Nadal")
+        }
+        assertEquals("Nadal : forty - Djokovic : love", gioco.getScore())
+    }
+
+    @Test
+    fun advantageGiocatore1() {
+        val gioco = Game("Nadal", "Djokovic")
+        for(i in 0..3){
+            gioco.newPoint("Nadal")
+        }
+        for(i in 0..2) {
+            gioco.newPoint("Djokovic")
+        }
+        assertEquals("advantage Nadal", gioco.getScore())
+    }
+
+    @Test
+    fun advantageGiocatore2() {
+        val gioco = Game("Nadal", "Djokovic")
+        for(i in 0..2){
+            gioco.newPoint("Nadal")
+        }
+        for(i in 0..3){
+            gioco.newPoint("Djokovic")
+        }
+        assertEquals("advantage Djokovic", gioco.getScore())
+    }
+
+    @Test
+    fun deucePerPareggioDopoTrePunti() {
+        val gioco = Game("Nadal", "Djokovic")
+        for(i in 0..4){
+            gioco.newPoint("Nadal")
+        }
+        for(i in 0..4){
+            gioco.newPoint("Djokovic")
+        }
+        assertEquals("deuce", gioco.getScore())
+    }
+
+    @Test
+    fun vittoriaPrimaDeiTrePunti() {
+        val gioco = Game("Nadal", "Djokovic")
+        for(i in 0..3){
+            gioco.newPoint("Nadal")
+        }
+        assertEquals("Nadal win!", gioco.getScore())
+    }
+
+    @Test
+    fun vittoriaDopoTrePunti() {
+        val gioco = Game("Nadal", "Djokovic")
+        for(i in 0..2){
+            gioco.newPoint("Nadal")
+        }
+        for(i in 0..4){
+            gioco.newPoint("Djokovic")
+        }
+        assertEquals("Djokovic win!", gioco.getScore())
+    }
+}
