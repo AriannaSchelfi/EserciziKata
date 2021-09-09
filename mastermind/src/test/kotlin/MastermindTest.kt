f30854759a5efad07d73b6db163776e9bb3af443
AriannaSchelfi <arianna.schelfi@hotmail.it>
2021-09-09 10:42:53 +0200

    correzioni

--- mastermind/src/test/kotlin/MastermindTest.kt
+++ mastermind/src/test/kotlin/MastermindTest.kt
@@ -1,58 +1,72 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-import org.junit.Test
-import kotlin.test.assertEquals
+internal class MastermindTest {
 
-class MastermindTest{
     @Test
     fun sequenzeVuote() {
         val mastermind = Mastermind()
+
         val result = mastermind.verifySequence(emptyList(), emptyList())
-        assertEquals(listOf(-1,-1),result)
+
+        assertEquals(listOf(-1, -1), result)
     }
 
     @Test
     fun sequenzeDiLunghezzeDiverseOttengoEccezzione() {
         val mastermind = Mastermind()
-        try {
+
+        val exception: java.lang.Exception = assertThrows(Exception::class.java) {
             mastermind.verifySequence(emptyList(), listOf("blue"))
-        }catch(e : Exception){
-            assertEquals("Lunghezze diverse non accettate", e.message)
         }
+
+        assertEquals("Lunghezze diverse non accettate", exception.message)
 
     }
 
     @Test
     fun sequenzeLunghezzaUnoNonIndovinata() {
         val mastermind = Mastermind()
+
         val result = mastermind.verifySequence(listOf("orange"), listOf("blue"))
-        assertEquals(listOf(0,0),result)
+
+        assertEquals(listOf(0, 0), result)
     }
 
     @Test
     fun sequenzeLunghezzaUnoIndovinata() {
         val mastermind = Mastermind()
+
         val result = mastermind.verifySequence(listOf("blue"), listOf("blue"))
-        assertEquals(listOf(1,0),result)
+
+        assertEquals(listOf(1, 0), result)
     }
 
     @Test
     fun sequenzeLunghezzaMaggioreDiUnoConSoloWellPlaced() {
         val mastermind = Mastermind()
-        val result = mastermind.verifySequence(listOf("blue","orange", "red"), listOf("black","orange", "green"))
-        assertEquals(listOf(1,0),result)
+
+        val result = mastermind.verifySequence(listOf("blue", "orange", "red"), listOf("black", "orange", "green"))
+
+        assertEquals(listOf(1, 0), result)
     }
 
     @Test
     fun sequenzeLunghezzaMaggioreDiUnoConSoloMissplaced() {
         val mastermind = Mastermind()
-        val result = mastermind.verifySequence(listOf("blue","orange", "red"), listOf("black", "green","orange"))
-        assertEquals(listOf(0,1),result)
+
+        val result = mastermind.verifySequence(listOf("blue", "orange", "red"), listOf("black", "green", "orange"))
+
+        assertEquals(listOf(0, 1), result)
     }
 
     @Test
     fun sequenzeLunghezzaMaggioreDiUnoConMissplacedEWellplaced() {
         val mastermind = Mastermind()
-        val result = mastermind.verifySequence(listOf("blue", "red", "green", "pink"), listOf("yellow", "red", "blue", "pink"))
-        assertEquals(listOf(2,1),result)
+
+        val result =
+            mastermind.verifySequence(listOf("blue", "red", "green", "pink"), listOf("yellow", "red", "blue", "pink"))
+
+        assertEquals(listOf(2, 1), result)
     }
 }
