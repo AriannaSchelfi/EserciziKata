3f04031edbf54e3e1286b24dbf1c0ede053f043b
AriannaSchelfi <arianna.schelfi@hotmail.it>
2021-09-09 10:49:25 +0200

    refactor move guards

--- mastermind/src/main/kotlin/Mastermind.kt
+++ mastermind/src/main/kotlin/Mastermind.kt
@@ -1,21 +1,19 @@
 //https://codingdojo.org/kata/Mastermind/
 
 class Mastermind {
+
     private var numberWellPlaced : Int =0
     private var numberMisplaced: Int =0
 
     fun verifySequence(secret : List<String>, guess : List<String>): List<Int>{
+        if (secret.size != guess.size) throw Exception("Lunghezze diverse non accettate")
+        if(secret.isEmpty()) return listOf(-1, -1)
 
-        if (secret.size != guess.size){
-            throw Exception("Lunghezze diverse non accettate")
-        }else if(secret.isNotEmpty()){
-            for (i in guess.indices){
-                verifyWellplaced(guess, i, secret)
-                verifiyMissplaced(guess, i, secret)
-            }
-            return listOf(numberWellPlaced, numberMisplaced)
+        for (i in guess.indices){
+            verifyWellplaced(guess, i, secret)
+            verifiyMissplaced(guess, i, secret)
         }
-        return listOf(-1,-1)
+        return listOf(numberWellPlaced, numberMisplaced)
     }
 
     private fun verifiyMissplaced(guess: List<String>, i: Int, secret: List<String>) {
